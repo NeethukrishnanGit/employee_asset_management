a2fd12416ae067fd6e8f500723b35db72a7f7115
ANORA\naveen <naveen@anorasolutions.com>
2023-06-09 16:05:11 +0530

    Edited delete,search, and updated endpoints in audit router

--- employee_asset_management/router/audit_trial.py
+++ employee_asset_management/router/audit_trial.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from pydantic import json
+from typing import Dict
 from bson.objectid import ObjectId
 from employee_asset_management.db_creator import Audit_Collection
 from employee_asset_management.model.audit_trial import Audit
@@ -9,20 +10,23 @@ json.ENCODERS_BY_TYPE[ObjectId] = str
 audit_app = APIRouter()
 
 
-@audit_app.post("/audit_data")
-async def read_data():
-    data = Audit_Collection.find()
-    return {"audit_data": list(data)}
+@audit_app.post("/audit_data/read/")
+async def read_data(value: Dict):
+    data = list(Audit_Collection.find(value))
+    return {"audit_data": data}
 
 
-@audit_app.delete("/audit_data/delete/{get_id}")
+@audit_app.delete("/audit_data/delete_one/")
 async def delete_data(get_id: str):
-    Audit_Collection.find_one_and_delete({"_id": ObjectId(get_id)})
-    data = list(Audit_Collection.find())
-    return {"After Deleted data": data}
+    try:
+        Audit_Collection.find_one_and_delete({"_id": ObjectId(get_id)})
+    except:
+        return "Something went wrong... Failed to Delete!"
+    else:
+        return "Successfully Deleted..."
 
 
-@audit_app.post("/audit_data/post/")
+@audit_app.post("/audit_data/insert_one/")
 async def insert_data(given_value: Audit):
     given_value.user_id = ObjectId(given_value.user_id)
     given_value.instrument_id = ObjectId(given_value.instrument_id)
@@ -31,10 +35,10 @@ async def insert_data(given_value: Audit):
     return {"inserted data": data}
 
 
-@audit_app.put("/audit_data/put/")
-async def update_data(my_id):
+@audit_app.put("/audit_data/update/{my_id}/")
+async def update_data(my_id: str, value: Dict):
     my_query = {"_id": ObjectId(my_id)}
-    update = {"$set": {"event_type": "check in"}}
+    update = {"$set": value}
     Audit_Collection.update_one(my_query, update)
-    data = list(Audit_Collection.find({"_id": ObjectId(my_id)}))
+    data = list(Audit_Collection.find(my_query))
     return {"updated data": data}
