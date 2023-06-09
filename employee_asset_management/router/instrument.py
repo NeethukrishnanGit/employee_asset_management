4ca207dc05a67b9dac621eceaaa865e3d5ed9538
Krithik2009 <krithik@anorasolutions.com>
2023-06-09 18:23:10 +0530

    update and delete endpoint in Instrument collection is edited

--- employee_asset_management/router/instrument.py
+++ employee_asset_management/router/instrument.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Body
 from pydantic import json
 from employee_asset_management.db_creator import Instrument_Collection
 from bson.objectid import ObjectId
@@ -11,7 +11,7 @@ instrument_app = APIRouter()
 
 
 @instrument_app.post(
-    "/instrument/",
+    "/get_instrument/",
     description="**To get An Instrument details specify the requirements inside the response body**",
     status_code=status.HTTP_302_FOUND
 )
@@ -20,24 +20,41 @@ async def get_instrument(search: Dict):
     return {"data": list(data)}
 
 
-@instrument_app.delete("/instrument/{instrument_id}")
-async def delete_instrument(instrument_id: str):
-    query = {"_id": ObjectId(instrument_id)}
-    Instrument_Collection.find_one_and_delete(query)
-    return "SUCCESSFULLY DELETED !!!"
+@instrument_app.delete("/")
+async def delete_instrument(instrument_id: str = Body(..., embed=True)):
+    try:
+        query = {"_id": ObjectId(instrument_id)}
+        Instrument_Collection.find_one_and_delete(query)
+    except:
+        return "Failed to delete"
+    else:
+        return "SUCCESSFULLY DELETED !!!"
 
 
-@instrument_app.post("/add_instrument/", response_model=Instruments)
+@instrument_app.post(
+    "/",
+    response_model=Instruments,
+    status_code=status.HTTP_201_CREATED,
+    response_description="**instrument is added**"
+)
 async def add_instrument(instrument: Instruments):
     data_dict = instrument.dict()
     result = Instrument_Collection.insert_one(data_dict)
     return {"id": str(result.inserted_id), **data_dict}
 
 
-@instrument_app.put("/instruments/{instrument_id}/")
-async def update_instrument_type(instrument_id: str, update_data: Dict):
+@instrument_app.put(
+    "/",
+    status_code=status.HTTP_200_OK,
+    response_description="UPDATE SUCCESSFUL"
+)
+async def update_instrument(*,
+                            instrument_id: str = Body(...),
+                            update_data: Dict
+                            ):
     query = {"_id": ObjectId(instrument_id)}
     update = {"$set": update_data}
     Instrument_Collection.update_one(query, update)
     updated_data = list(Instrument_Collection.find(query))
     return {"updated_data": updated_data}
+
