75270073dbd7961aa3d9f5d41b895a48c19affe1
Krithik2009 <krithik@anorasolutions.com>
2023-06-09 16:10:34 +0530

    search and update operations in Instrument Router is edited

--- employee_asset_management/router/instrument.py
+++ employee_asset_management/router/instrument.py
@@ -1,18 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from pydantic import json
 from employee_asset_management.db_creator import Instrument_Collection
 from bson.objectid import ObjectId
 from employee_asset_management.model.instrument import Instruments
+from typing import Dict
 
 json.ENCODERS_BY_TYPE[ObjectId] = str
 
 instrument_app = APIRouter()
 
 
-@instrument_app.post("/instrument/{instrument_id}")
-async def get_instrument(instrument_id: str):
-    query = {"_id": ObjectId(instrument_id)}
-    data = Instrument_Collection.find(query)
+@instrument_app.post(
+    "/instrument/",
+    description="**To get An Instrument details specify the requirements inside the response body**",
+    status_code=status.HTTP_302_FOUND
+)
+async def get_instrument(search: Dict):
+    data = Instrument_Collection.find(search)
     return {"data": list(data)}
 
 
@@ -30,13 +34,10 @@ async def add_instrument(instrument: Instruments):
     return {"id": str(result.inserted_id), **data_dict}
 
 
-@instrument_app.put("/instruments/{instrument_id}/{type_change}", response_model=Instruments)
-async def update_instrument_type(instrument_id: str, type_change: str):
+@instrument_app.put("/instruments/{instrument_id}/")
+async def update_instrument_type(instrument_id: str, update_data: Dict):
     query = {"_id": ObjectId(instrument_id)}
-    update = {"$set": {"type": type_change}}
-    stored_data = Instrument_Collection.update_one(query, update)
-
-    if stored_data.matched_count:
-        return {"message": "update successful"}
-    else:
-        return {"message": "Data not found"}
+    update = {"$set": update_data}
+    Instrument_Collection.update_one(query, update)
+    updated_data = list(Instrument_Collection.find(query))
+    return {"updated_data": updated_data}
