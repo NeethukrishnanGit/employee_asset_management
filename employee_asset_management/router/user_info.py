623451a750d2bf04159126e2a72faf5bc1ed2e74
Krithik2009 <krithik@anorasolutions.com>
2023-06-13 14:44:52 +0530

    expections for user_data is added

--- employee_asset_management/router/user_info.py
+++ employee_asset_management/router/user_info.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, HTTPException
 from pydantic import json
 from typing import Dict
-from employee_asset_management.get_collection import user_Collection, instrument_Collection, audit_trail_Collection
+from employee_asset_management.get_collection import user_Collection
 from employee_asset_management.model.user_info import User
 from bson.objectid import ObjectId
-from datetime import datetime
+from bson.errors import InvalidId
+
 
 json.ENCODERS_BY_TYPE[ObjectId] = str
 
@@ -13,6 +14,9 @@ user_app = APIRouter()
 
 @user_app.post("/read_user_data")
 async def get_user_data(data: Dict):
+    for i in data.keys():
+        if i == "_id":
+            data[i] = ObjectId(data[i])
     user = user_Collection.find(data)
     user_data = list(user)
     return {"user_data": user_data}
@@ -21,9 +25,11 @@ async def get_user_data(data: Dict):
 @user_app.delete("/delete_user_data")
 async def delete_user_data(user_id: str = Body(..., embed=True)):
     try:
-        user_Collection.find_one_and_delete({"_id": ObjectId(user_id)})
-    except:
-        return "Something went wrong... Deletion Failed!"
+        data = user_Collection.find_one_and_delete({"_id": ObjectId(user_id)})
+        if data is None:
+            raise HTTPException(status_code=404, detail="user not found")
+    except InvalidId as e:
+        raise HTTPException(status_code=400, detail=str(e))
     else:
         return "Successfully Deleted..."
 
@@ -37,8 +43,14 @@ async def create_user_data(user: User):
 
 @user_app.put("/update_user_data")
 async def update_user_data(user_id: str = Body(...), role_change: str = Body(...)):
-    id_doc = {"_id": ObjectId(user_id)}
-    set_doc = {"$set": {"role": role_change}}
-    user_Collection.update_one(id_doc, set_doc)
-    data = list(user_Collection.find(id_doc))
-    return {"updated data": data}
+    try:
+        id_doc = {"_id": ObjectId(user_id)}
+        set_doc = {"$set": {"role": role_change}}
+        data = user_Collection.update_one(id_doc, set_doc)
+        if data.matched_count:
+            updated_data = list(user_Collection.find(id_doc))
+            return {"updated data": updated_data}
+        else:
+            raise HTTPException(status_code=404, detail="user not found")
+    except InvalidId as e:
+        raise HTTPException(status_code=400, detail=str(e))
